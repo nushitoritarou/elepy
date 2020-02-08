28dce151fbab90924dce3a5d34b7e1340630ac2e
nushitoritarou <doragonwide@gmail.com>
2020-02-08 23:51:30 +0900

    code calc_RC

--- elepy.py
+++ elepy.py
@@ -2,6 +2,53 @@ import math
 import numpy as np
 from matplotlib import pyplot as plt
 
+
+x = np.arange(10,19.5,0.1)
+e = [math.sin(i) for i in x]
+
+
+
+def calc_RL(r,l,d):
+    pass
+
+def calc_RC(r,c,q):
+    i=[0 for i in x]
+    vq=[0 for i in x]
+    qt=q
+    for j, ele in enumerate(e):
+        i[j]=(ele-qt)/c/r
+        
+        vq[j]=qt/c
+        before_q=qt
+        qt+=i[j]
+
+        if abs(before_q)<abs(ele) and abs(qt)>abs(ele):
+            qt=ele*c
+           # print(str(j*0.1) +'s  ele*c = '+str(vq[j]))
+    
+    fig,ax1 = plt.subplots()
+
+    ax1.plot(x,e,color='tab:orange',label='Volt')
+    ax2 = ax1.twinx()
+    ax2.plot(x,i,color='tab:green',label = 'anpare')
+    plt.title('RC')
+
+    #ax3 = ax2.twinx()
+    #ax1.plot(x,vq,color='tab:red',label = 'Vc')
+    #ax1.legend(loc=0)
+    
+    plt.show()
+
+
+def calc_RLC():
+    pass
+
+if __name__=='__main__':
+    calc_RC(10,0.1,0)
+
+
+
+
 def data_plot(x,y,z):
     #fig = plt.figure(figsize=(10,6))
     fig,ax1 = plt.subplots()
@@ -20,6 +67,3 @@ def data_plot(x,y,z):
     plt.ylabel('Y軸ラベル')
 
     plt.show()
-
-if __name__=='__main__':
-    data_plot()
