f0ac6729ee98d9cf1e250c869a896847a68ad000
nushitoritarou <doragonwide@gmail.com>
2020-02-08 21:28:18 +0900

    plot 2 datas

--- elepy.py
+++ elepy.py
@@ -2,15 +2,24 @@ import math
 import numpy as np
 from matplotlib import pyplot as plt
 
-def main():
-    fig = plt.figure(figsize=(10,6))
+def data_plot(x,y,z):
+    #fig = plt.figure(figsize=(10,6))
+    fig,ax1 = plt.subplots()
 
-    x = np.arange(0,10,0.1)
+    x = np.arange(0,10,0.1)     #?　0-10 を0.1 幅
     y= [math.sin(i) for i in x]
+    z= [5*math.cos(i) for i in x]
 
-    plt.plot(x,y)
+    ax1.plot(x,y,color='tab:orange')
+
+    ax2 = ax1.twinx()
+
+    ax2.plot(x,z, color='tab:green')
+    plt.title('title')
+    plt.xlabel('X軸ラベル')
+    plt.ylabel('Y軸ラベル')
 
     plt.show()
 
 if __name__=='__main__':
-    main()
+    data_plot()
